db18420929e7229cb100ad1742ead8fa91d5b28d
Joshua Paul <joshpauljtp@gmail.com>
2024-12-15 01:59:34 +0530

    Fixed desc

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -5,10 +5,8 @@ export const CLUSTERS = [
   {
     label: "Alpha",
     link: "/alpha",
-    desc: `Inspired by BlackBox Infinite's rendition of the Ferrari
-              Testarossa's instrument cluster using Apple Carplay's Design
-              Framework, with a few creative liberties here and there. Made with
-              Rive.`,
+    desc: `Retro-infusion, for the wayfarers of the future. First cluster
+              created for project Rubicon. Made with code.`,
     img: AlphaPreview,
   },
   // {
@@ -18,8 +16,10 @@ export const CLUSTERS = [
   {
     label: "Testarossa",
     link: "/testarossa",
-    desc: `Retro-infusion, for the wayfarers of the future. First cluster
-              created for project Rubicon. Made with code.`,
+    desc: `Inspired by BlackBox Infinite's rendition of the Ferrari
+                        Testarossa's instrument cluster using Apple Carplay's Design
+                        Framework, with a few creative liberties here and there. Made with
+                        Rive.`,
     img: TestarossaPreview,
   },
 ];
