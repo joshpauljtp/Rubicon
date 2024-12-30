1767ae7d40473282dc152efe324c8bebb5cadcd0
Joshua Paul <joshpauljtp@gmail.com>
2024-12-30 15:53:08 +0530

    Wasm preload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Analytics } from "@vercel/analytics/react";
 import Navigator from "./components/Navigator";
+
+import riveWASMResource from "@rive-app/canvas/rive.wasm";
 function App() {
   return (
     <>
+      <link
+        rel="preload"
+        href={riveWASMResource}
+        as="fetch"
+        crossOrigin="anonymous"
+      />
       <Analytics />
       <Navigator />
     </>
