0158312e76fee60056ac8207483275078be27dcf
Joshua Paul <joshpauljtp@gmail.com>
2024-12-30 20:02:13 +0530

    Binary data parsing

--- src/components/useConfig.ts
+++ src/components/useConfig.ts
@@ -6,8 +6,10 @@ const RPM_MULTIPLIER = 2;
 const SPEED_MULTIPLIER = 0.05;
 
 function useConfig() {
-  const socketUrl = "ws://192.168.125.207:80/ws";
-  const { readyState, lastMessage } = useWebSocket(socketUrl);
+  const socketUrl = `ws://192.168.116.227:80/ws`;
+  const { readyState, lastMessage, getWebSocket } = useWebSocket(socketUrl, {
+    share: true,
+  });
   const [isLoading, setIsLoading] = useState(false);
 
   const [config, setConfig] = useState<Config>({
@@ -15,11 +17,11 @@ function useConfig() {
     rpm: 0,
     indicatorLeft: false,
     indicatorRight: false,
-    abs: true,
+    abs: false,
     service: false,
     highBeam: false,
-    neutral: false,
-    fuel: 100,
+    neutral: true,
+    fuel: 30,
   });
 
   const [currentRpm, setCurrentRpm] = useState(0); // For smooth transitions
@@ -27,6 +29,49 @@ function useConfig() {
 
   const [currentSpeed, setCurrentSpeed] = useState(0); // For smooth transitions
   const [targetSpeed, setTargetSpeed] = useState(0);
+
+  // useEffect(() => {
+  //   const ws = new WebSocket(socketUrl);
+
+  //   ws.binaryType = "arraybuffer"; // Ensure binary data is received as ArrayBuffer
+
+  //   ws.onmessage = (event) => {
+  //     // Ensure the received data is an ArrayBuffer
+  //     const buffer = event.data;
+
+  //     // Parse the binary data using DataView
+  //     const dataView = new DataView(buffer);
+
+  //     // Extract the data in the same order it was packed on the server
+  //     const rpm =
+  //       (dataView.getUint8(0) << 24) |
+  //       (dataView.getUint8(1) << 16) |
+  //       (dataView.getUint8(2) << 8) |
+  //       dataView.getUint8(3);
+
+  //     const highBeam = (dataView.getUint8(4) << 8) | dataView.getUint8(5);
+  //     const rightIndicator = (dataView.getUint8(6) << 8) | dataView.getUint8(7);
+  //     const leftIndicator = (dataView.getUint8(8) << 8) | dataView.getUint8(9);
+
+  //     // Log or use the parsed data
+  //     console.log({
+  //       rpm,
+  //       highBeam,
+  //       rightIndicator,
+  //       leftIndicator,
+  //     });
+
+  //     // Update React state or UI as needed
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    //Change binaryType property of WebSocket
+    if (!getWebSocket()) return;
+    (getWebSocket() as any).binaryType = "arraybuffer";
+
+    console.log((getWebSocket() as any).binaryType, "wasd");
+  }, []);
 
   useEffect(() => {
     let timerId: any;
@@ -44,21 +89,47 @@ function useConfig() {
   }, [readyState]);
 
   useEffect(() => {
-    const data = JSON.parse(lastMessage?.data ?? "{}");
-    console.log("data", data);
+    if (!lastMessage) return;
+    // console.log(lastMessage);
+    lastMessage.data.arrayBuffer().then((res: any) => {
+      if (res) {
+        const data = new DataView(res);
 
-    setConfig((prev) => ({
-      ...prev,
-      indicatorLeft: data?.p34 > 3000,
-      indicatorRight: data?.p35 > 3000,
-    }));
+        // Extract the data in the same order it was packed on the server
+        const rpm =
+          (data.getUint8(0) << 24) |
+          (data.getUint8(1) << 16) |
+          (data.getUint8(2) << 8) |
+          data.getUint8(3);
 
-    setTargetRpm(
-      (data ? Number(data?.p33 > 500 ? data?.p33 : 0) : 0) * RPM_MULTIPLIER
-    ); // Update target RPM
-    setTargetSpeed(
-      Math.ceil((data ? Number(data?.svx ?? 0) : 0) * SPEED_MULTIPLIER)
-    ); // Update target Speed
+        const highBeam = (data.getUint8(4) << 8) | data.getUint8(5);
+        const rightIndicator = (data.getUint8(6) << 8) | data.getUint8(7);
+        const leftIndicator = (data.getUint8(8) << 8) | data.getUint8(9);
+
+        // Log or use the parsed data
+        console.log({
+          rpm,
+          highBeam,
+          rightIndicator,
+          leftIndicator,
+        });
+
+        setConfig((prev) => ({
+          ...prev,
+          highBeam: highBeam > 500,
+          indicatorLeft: leftIndicator > 500,
+          indicatorRight: rightIndicator > 500,
+        }));
+
+        setTargetRpm(rpm - 2000); // Update target RPM
+      }
+    });
+
+    // console.log("data", data);
+
+    // setTargetSpeed(
+    //   Math.ceil((data ? Number(data?.svx ?? 0) : 0) * SPEED_MULTIPLIER)
+    // ); // Update target Speed
   }, [lastMessage]);
 
   useEffect(() => {
