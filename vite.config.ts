f07d01073205b79aa5a0dcc0a5139f34b8858ab6
Joshua Paul <joshpauljtp@gmail.com>
2024-12-30 15:52:59 +0530

    wasm support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,45 @@
 import react from "@vitejs/plugin-react";
+import * as fs from "fs";
+import path from "path";
 import { defineConfig } from "vite";
+
+// Custom middleware to serve wasm files with the correct MIME type
+const wasmMiddleware = () => {
+  return {
+    name: "wasm-middleware",
+    configureServer(server: any) {
+      server.middlewares.use((req: any, res: any, next: any) => {
+        if (req.url.endsWith(".wasm")) {
+          const wasmPath = path.join(
+            __dirname,
+            "node_modules/@rive-app/canvas/",
+            path.basename(req.url)
+          );
+          const wasmFile = fs.readFileSync(wasmPath);
+          res.setHeader("Content-Type", "application/wasm");
+          res.end(wasmFile);
+          return;
+        }
+        next();
+      });
+    },
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ["**/*.riv"],
+  plugins: [react(), wasmMiddleware()],
+  assetsInclude: ["**/*.riv", "**/*.wasm"],
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      supported: {
+        "top-level-await": true,
+      },
+    },
+  },
 });
