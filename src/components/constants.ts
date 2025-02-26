7540a548ca788b22d69c24ae15d615fefe1016c9
Joshua Paul <joshpauljtp@gmail.com>
2025-02-26 21:38:57 +0530

    Swapping positions

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -3,17 +3,6 @@ import TestarossaPreview from "../assets/Info/TestarossaPreview.png";
 
 export const CLUSTERS = [
   {
-    label: "Alpha",
-    link: "/alpha",
-    desc: `Retro-infusion, for the wayfarers of the future. First cluster
-              created for project Rubicon. Made with code.`,
-    img: AlphaPreview,
-  },
-  // {
-  //   label: "Funk",
-  //   link: "/funk",
-  // },
-  {
     label: "Testarossa",
     link: "/testarossa",
     desc: `Inspired by BlackBox Infinite's rendition of the Ferrari
@@ -21,5 +10,12 @@ export const CLUSTERS = [
                         Framework, with a few creative liberties here and there. Made with
                         Rive.`,
     img: TestarossaPreview,
+  },
+  {
+    label: "Alpha",
+    link: "/alpha",
+    desc: `Retro-infusion, for the wayfarers of the future. First cluster
+              created for project Rubicon. Made with code.`,
+    img: AlphaPreview,
   },
 ];
